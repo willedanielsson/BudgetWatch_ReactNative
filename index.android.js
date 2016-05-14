ddffe782ff783b8864ef561ed1d8f4569b78d1c2
William Danielsson <william.danielsson@clm-194fvh8.local>
2016-05-14 14:56:58 +0200

    Navigator with custom toolbar

--- index.android.js
+++ index.android.js
@@ -14,6 +14,7 @@ import {
   StyleSheet,
   Text,
   View,
+  Navigator,
   Image,
   TouchableHighlight
 } from 'react-native';
@@ -23,7 +24,29 @@ import {
 var ToolbarAndroid = require('ToolbarAndroid');
 
 var BudgetWatch_ReactNative = React.createClass({
-  getInitialState: function() {
+  renderScene(route, navigator){
+    if(route.name == 'Main'){
+      return (
+        <Main navigator={navigator} {...route.passProps}/>
+        )
+    }
+    if(route.name == 'Home'){
+      return <Home navigator={navigator} {...route.passProps} />
+    }
+  },
+
+  render() {
+    return (
+      <Navigator
+        style={{flex:1}}
+        initialRoute={{ name: 'Main'}}
+        renderScene={this.renderScene} />
+    )
+  }
+});
+
+var Toolbar = React.createClass({
+getInitialState: function() {
     return {
       colorProps: {
         titleColor: '#FFFFFF',
@@ -31,87 +54,80 @@ var BudgetWatch_ReactNative = React.createClass({
       },
     };
   },
-
   render: function() {
     return (
-      <View style={styles.container}>
-
-        <ToolbarAndroid 
-          style={styles.toolbar} 
-          title="AwesomeApp" 
-          {...this.state.colorProps}
-        />
-        <View style={styles.listContainer}>
-        <TouchableHighlight onPress={this._onPressButton(1)}>
-          <View style={styles.item}>
-            <Image
-              style={styles.icon}
-              source={require('./images/purse.png')}/>
-            <View style={styles.textContainer}>
-
-              <Text style={styles.header}>Budgets</Text>
-              <Text>Create and manage budgets</Text>
-
-            </View>
-          </View>
-        </TouchableHighlight>
-
-          <View style={styles.item}>
-            <Image
-              style={styles.icon}
-              source={require('./images/transaction.png')}/>
-            <View style={styles.textContainer}>
-
-              <Text style={styles.header}>Transactions</Text>
-              <Text>Enter transactions and revenues</Text>
-
-            </View>
-          </View>
-        
-        </View>
-
-      </View>
-    );
-  },
-
-  _onPressButton: function(input){
-    console.log("Hejdå");
+      <ToolbarAndroid 
+        style={styles.toolbar} 
+        title="AwesomeApp" 
+        {...this.state.colorProps}
+      />
+    )
   }
-  
+
 });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
+var Main = React.createClass({
+  _navigate(name){
+    this.props.navigator.push({
+      name: 'Home',
+      passProps: {
+        name: name
+      }
+    })
   },
+  render(){
+    return(
+      <View style={styles.container}>
+      <Toolbar />
+        <Text style={styles.heading}>
+          Hello from Main
+        </Text>
+        <TouchableHighlight style={styles.button} onPress={ () => this._navigate('yoyoyo')}>
+          <Text style={styles.buttonText}>Go</Text>
+        </TouchableHighlight>  
+      </View>
+    )
+  }
+});
+
+
+var Home = React.createClass({
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Hi from {this.props.name}
+        </Text>
+        <TouchableHighlight style={styles.button} onPress={ () => this.props.navigator.pop()}>
+          <Text style={styles.buttonText}>Back</Text>
+        </TouchableHighlight>  
+      </View>
+    )
+  }
+})
+
+var styles = StyleSheet.create({
   toolbar: {
     height: 56,
     backgroundColor: '#3F51B5'
   },
-  listContainer: {
-    padding: 16
+  container: {
+    flex:1
   },
-  item: {
-    paddingTop: 10,
-    paddingBottom: 10,
-    flexDirection: 'row',
-    borderBottomColor: '#DDDDDD',
-    borderBottomWidth: 1,
+  heading: {
+    fontSize:22,
+    marginBottom:10
   },
-  icon: {
-   height: 40,
-   width: 40,
+  button: {
+    height: 60,
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    alignItems: 'center'
   },
-  textContainer: {
-    paddingLeft: 10,
-    paddingRight: 10,
-    flex: 1,
-  },
-  header: {
+  buttonText: {
     fontSize: 20
-  },
+  }
+
 });
 
 AppRegistry.registerComponent('BudgetWatch_ReactNative', () => BudgetWatch_ReactNative);
